c5d5e6f770abfa7719049c02779b1790728482e8
YuliaBardakova <108867938+YuliaBardakova@users.noreply.github.com>
2022-12-09 14:30:20 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryCards(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+imagesContainer.addEventListener('click', onClick);
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
+
+function onClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+    return;
+    }
+
+console.log(evt.target.nodeName);
+}
+
+function createGalleryCards(images) {
+    return images
+    .map(({ preview, original, description }) => {
+    return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
